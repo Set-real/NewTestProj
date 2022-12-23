54f402904113b9fad1696707950916afa8d1523b
Name <email@example.ru>
2022-12-23 13:40:21 +0300

    Добавьте файлы проекта.

---
+++ Calc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewTestProj
+{
+    public class Calc
+    {
+        public int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
